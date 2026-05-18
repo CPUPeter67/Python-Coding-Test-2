19a71a250300d8aff8b3d9cf7dd5c6433a45514b
CPUPetet <c.nwodo@abujaprep.com>
2026-05-18 17:30:01 +0100

    FunctionCalculatorTest

---
+++ FunctionCalculator.py
@@ -0,0 +1,23 @@
+def addition(a, b):
+    return(a + b)
+def subtraction(a, b):
+    return(a - b)
+def multiplication(a, b):
+    return(a * b)
+def division(a, b):
+    return(a / b)
+
+number1 = float(input("Enter the value for the first number: "))
+number2 = float(input("Enter the value for the second number: "))
+
+print("The addition function's result is: ", addition(number1, number2))
+print("The subtraction function's result is: ", subtraction(number1, number2))
+print("The multiplication function's result is: ", multiplication(number1, number2))
+print("The division function's result is: ", division(number1, number2))
+
+try:
+    division(number1, number2)
+except ZeroDivisionError:
+    print("You cannot divide by zero. You will always achieve zero!")
+except ValueError:
+    print("Your input is not numerical! Please enter a different number.")    
